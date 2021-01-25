b6540c100612a609307513e66fc00415c1021f8e
nhlinh123 <nhlinh123@gmail.com>
2021-01-25 14:38:01 +0700

    Intitial commit

--- JS/Animatedbanner/animatedbanner.js
+++ JS/Animatedbanner/animatedbanner.js
@@ -5,5 +5,5 @@ for (var i = 0; i < 400; i++) {
     banner.innerHTML += "<div class='blocks'></div>";
     const duration = Math.random() * 5;
     blocks[i].style.animationDuration = 2+ duration + 's';
-    blocks[i].style.animationDelay = duration + 's';
+    blocks[i].style.animationDelay = duration + 's' ;
 }
