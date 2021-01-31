11e457c6266738844f5a9a2344ce2e95e235cc9f
nhlinh123 <nhlinh123@gmail.com>
2021-01-31 17:59:56 +0700

    main

--- JS/Animatedbanner/animatedbanner.js
+++
@@ -1,9 +0,0 @@
-const banner = document.getElementsByClassName("banner")[0];
-const blocks = document.getElementsByClassName("blocks");
-
-for (var i = 0; i < 400; i++) {
-    banner.innerHTML += "<div class='blocks'></div>";
-    const duration = Math.random() * 5;
-    blocks[i].style.animationDuration = 2+ duration + 's';
-    blocks[i].style.animationDelay = duration + 's' ;
-}
